8a2db15cfc2df5cbec46139bef03b8ec22cb60be
Jackson Emmerich <jacksonemmerich@gmail.com>
2023-05-09 23:29:07 -0400

    add import.sql

--- src/main/java/com/jacksonemmerich/dslist/entities/Game.java
+++ src/main/java/com/jacksonemmerich/dslist/entities/Game.java
@@ -21,27 +21,34 @@ public class Game {
 	@Column(name = "game_year")
 	private Integer year;
 	private String genre;
-	private String plataforms;
-	//private double score;
+	private String platforms;
+	private Double score;
 	private String imgUrl;
+	@Column(columnDefinition = "TEXT")
 	private String shortDescription;
+	@Column(columnDefinition = "TEXT")
 	private String longDescription;
 	
 	public Game() {
 		
 	}
 
-	public Game(Long id, String title, Integer year, String genre, String plataforms, String imgUrl,
+	
+
+	public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
 			String shortDescription, String longDescription) {
 		this.id = id;
 		this.title = title;
 		this.year = year;
 		this.genre = genre;
-		this.plataforms = plataforms;
+		this.platforms = platforms;
+		this.score = score;
 		this.imgUrl = imgUrl;
 		this.shortDescription = shortDescription;
 		this.longDescription = longDescription;
 	}
+
+
 
 	public Long getId() {
 		return id;
@@ -75,13 +82,19 @@ public class Game {
 		this.genre = genre;
 	}
 
-	public String getPlataforms() {
-		return plataforms;
+	
+
+	public String getPlatforms() {
+		return platforms;
 	}
 
-	public void setPlataforms(String plataforms) {
-		this.plataforms = plataforms;
+
+
+	public void setPlatforms(String platforms) {
+		this.platforms = platforms;
 	}
+
+
 
 	public String getImgUrl() {
 		return imgUrl;
@@ -106,6 +119,19 @@ public class Game {
 	public void setLongDescription(String longDescription) {
 		this.longDescription = longDescription;
 	}
+	
+	
+	public Double getScore() {
+		return score;
+	}
+
+
+
+	public void setScore(Double score) {
+		this.score = score;
+	}
+
+
 
 	@Override
 	public int hashCode() {
