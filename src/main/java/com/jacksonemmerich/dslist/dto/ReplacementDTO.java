f5cc459f863c891423d25e46a2e69423ab262909
Jackson Emmerich <jacksonemmerich@gmail.com>
2023-05-15 22:32:00 -0400

    projeto finalizado

---
+++ src/main/java/com/jacksonemmerich/dslist/dto/ReplacementDTO.java
@@ -0,0 +1,25 @@
+package com.jacksonemmerich.dslist.dto;
+
+public class ReplacementDTO {
+	
+	private Integer sourceIndex;
+	private Integer destinationIndex;
+	
+	public Integer getDestinationIndex() {
+		return destinationIndex;
+	}
+	public void setDestinationIndex(Integer destinationIndex) {
+		this.destinationIndex = destinationIndex;
+	}
+	public Integer getSourceIndex() {
+		return sourceIndex;
+	}
+	public void setSourceIndex(Integer sourceIndex) {
+		this.sourceIndex = sourceIndex;
+	}
+	
+	 
+	
+	
+
+}
